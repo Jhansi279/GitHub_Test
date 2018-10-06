b50211934628074af449f6b669c2ff8be71c177b
Jhansi <ranijhansi.g@gmail.com>
2018-10-06 07:54:05 -0700

    first Commit

---
+++ src/test/java/com/MavenPrac/maven/Test1.java
@@ -0,0 +1,26 @@
+package com.MavenPrac.maven;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void sum() {
+		System.out.println("Sum");
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("Sub");
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("Mul");
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("Div");
+	}
+}
---
+++ src/test/java/com/MavenPrac/maven/Test2.java
@@ -0,0 +1,27 @@
+package com.MavenPrac.maven;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void sum2() {
+		System.out.println("Sum2");
+	}
+	
+	@Test
+	public void sub2() {
+		System.out.println("Sub2");
+	}
+	
+	@Test
+	public void mul2() {
+		System.out.println("Mul2");
+	}
+	
+	@Test
+	public void div2() {
+		System.out.println("Div2");
+	}
+
+}
